cbc4466b23e350d8f0fd801bd57157168890174a
Marin <mrn.aglic@gmail.com>
2016-05-08 15:32:36 +0200

    Added instruction and procedure classes

---
+++ src/main/scala/astgenerator/Instruction.scala
@@ -0,0 +1,13 @@
+package astgenerator
+
+/**
+  * Created by Marin on 08/05/16.
+  */
+trait Instruction
+trait BodyInstruction
+
+case class ProcedureCallExtraInput(name: String, variables: List[Variable], expressions: List[Expression]) extends BodyInstruction with Instruction
+case class ProcedureCall(name: String, variables: List[Variable], expressions: List[Expression]) extends BodyInstruction with Instruction
+
+case class Body(bodyInstruction: List[BodyInstruction]) extends Instruction
+case class ProcedureDef(name: String, variables: List[Variable], body: Body) extends Instruction
---
+++ src/main/scala/astgenerator/Procedure.scala
@@ -0,0 +1,7 @@
+package astgenerator
+
+/**
+  * Created by Marin on 08/05/16.
+  */
+
+abstract class Procedure
